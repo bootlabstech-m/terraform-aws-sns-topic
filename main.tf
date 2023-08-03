8a73cda2d45f1bbc312a22ded37ce0919aaba1ae
Silpashree Acharya <silpashree@bootlabstech.com>
2023-08-03 13:13:17 +0530

    added lifecycle block

--- main.tf
+++ main.tf
@@ -3,4 +3,7 @@ resource "aws_sns_topic" "sns_topic" {
   name            = var.sns_topic_name[count.index]
   delivery_policy = var.delivery_policy
   fifo_topic      = var.fifo_topic
+    lifecycle {
+    ignore_changes = [tags]
+  }
 }
