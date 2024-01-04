540926f8541a75514636df443162da3d12c500ca
Silpashree Acharya <silpashree@bootlabstech.com>
2024-01-04 12:35:39 +0530

    countfix,FIFO name fix and encryption

--- main.tf
+++ main.tf
@@ -1,6 +1,8 @@
 resource "aws_sns_topic" "sns_topic" {
-  count           = var.no_of_sns_topics
-  name            = var.sns_topic_name[count.index]
-  delivery_policy = var.delivery_policy
-  fifo_topic      = var.fifo_topic
+  count                       = var.no_of_sns_topics
+  name                        = var.fifo_topic ? "${var.sns_topic_name}-${count.index + 1}.fifo" : "${var.sns_topic_name}-${count.index + 1}"
+  delivery_policy             = var.delivery_policy
+  fifo_topic                  = var.fifo_topic
+  content_based_deduplication = var.fifo_topic ? var.content_based_deduplication : null
+  kms_master_key_id           = var.kms_master_key_id
 }
--- variables.tf
+++ variables.tf
@@ -4,15 +4,15 @@ variable "region" {
 }
 variable "role_arn" {
   description = " The ARN of the IAM role"
-  type = string
+  type        = string
 }
 variable "no_of_sns_topics" {
   description = "no_of_sns_topics to create"
   type        = number
 }
 variable "sns_topic_name" {
-  description = "sns_topic_names to create"
-  type        = list(string)
+  description = "sns_topic_names to create.Apeend the name with .fifo if creating FIFO queues."
+  type        = string
 }
 variable "delivery_policy" {
   description = "delivery_policy"
@@ -21,4 +21,12 @@ variable "delivery_policy" {
 variable "fifo_topic" {
   description = "whether fifo topic"
   type        = bool
+}
+variable "content_based_deduplication" {
+  description = "content_based_deduplication"
+  type        = bool
+}
+variable "kms_master_key_id" {
+  description = "kms_master_key_id"
+  type        = string
 }
