90157594b80e462c31ce0f7b915f0b2bcd5443e7
Bhargava Naidu <bhargava@bootlabstech.com>
2025-06-11 11:23:38 +0530

    updated the variables

--- variables.tf
+++ variables.tf
@@ -12,7 +12,7 @@ variable "no_of_sns_topics" {
 }
 variable "sns_topic_name" {
   description = "sns_topic_names to create.Apeend the name with .fifo if creating FIFO queues."
-  type        = list(string)
+  type        = string
 }
 variable "delivery_policy" {
   description = "delivery_policy"
