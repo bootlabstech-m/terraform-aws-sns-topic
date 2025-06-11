69e23d2c46ec0ef581bfee7ce303b22e718de7d6
Bhargava Naidu <bhargava@bootlabstech.com>
2025-06-11 11:01:07 +0530

    update lifecycl block

--- main.tf
+++ main.tf
@@ -7,6 +7,13 @@ resource "aws_sns_topic" "sns_topic" {
   kms_master_key_id           = var.kms_master_key_id
   tracing_config              = var.tracing_config
   lifecycle {
-    ignore_changes = [tags,delivery_policy,archive_policy]
+    ignore_changes = [
+      tags,
+      delivery_policy,
+      archive_policy,
+      sqs_failure_feedback_role_arn,
+      sqs_success_feedback_role_arn,
+      sqs_success_feedback_sample_rate
+      ]
   }
 }
