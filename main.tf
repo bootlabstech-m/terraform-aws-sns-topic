afccfc2d24181d80aa34b6a44fd44efc20a7ebfb
Bhargava Naidu <bhargava@bootlabstech.com>
2025-07-07 15:11:55 +0530

    new provider and cli version 5.46.0

--- main.tf
+++ main.tf
@@ -1,11 +1,12 @@
 resource "aws_sns_topic" "sns_topic" {
   count                       = var.no_of_sns_topics
-  name                        = var.fifo_topic ? "${var.sns_topic_name}-${count.index + 1}.fifo" : "${var.sns_topic_name}-${count.index + 1}"
-  delivery_policy             = var.delivery_policy
+  name                        = "${var.sns_topic_name}-${count.index + 1}${var.fifo_topic ? ".fifo" : ""}"
   fifo_topic                  = var.fifo_topic
   content_based_deduplication = var.fifo_topic ? var.content_based_deduplication : null
+  delivery_policy             = var.fifo_topic ? null : var.delivery_policy
   kms_master_key_id           = var.kms_master_key_id
   tracing_config              = var.tracing_config
+
   lifecycle {
     ignore_changes = [
       tags,
@@ -13,7 +14,7 @@ resource "aws_sns_topic" "sns_topic" {
       archive_policy,
       sqs_failure_feedback_role_arn,
       sqs_success_feedback_role_arn,
-      sqs_success_feedback_sample_rate
-      ]
+      sqs_success_feedback_sample_rate,
+    ]
   }
 }
--- variables.tf
+++ variables.tf
@@ -14,10 +14,28 @@ variable "sns_topic_name" {
   description = "sns_topic_names to create.Apeend the name with .fifo if creating FIFO queues."
   type        = string
 }
+
 variable "delivery_policy" {
-  description = "delivery_policy"
+  description = "Delivery policy for the SNS topic."
   type        = string
+  default     = <<EOT
+{
+  "http": {
+    "defaultHealthyRetryPolicy": {
+      "minDelayTarget": 20,
+      "maxDelayTarget": 20,
+      "numRetries": 3,
+      "numMaxDelayRetries": 0,
+      "numNoDelayRetries": 0,
+      "numMinDelayRetries": 0,
+      "backoffFunction": "linear"
+    },
+    "disableSubscriptionOverrides": false
+  }
 }
+EOT
+}
+
 variable "fifo_topic" {
   description = "whether fifo topic"
   type        = bool
